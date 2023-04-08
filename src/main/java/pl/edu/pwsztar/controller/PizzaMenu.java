fafa299bdccad6f442769506c3e112afe10c2104
Dawid Durołek <dawid.durolek@onet.pl>
2023-04-08 22:06:32 +0200

    inicialize work on pizza-menu

---
+++ src/main/java/pl/edu/pwsztar/controller/PizzaMenu.java
@@ -0,0 +1,15 @@
+package pl.edu.pwsztar.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping(value="/api")
+public class PizzaMenu {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PizzaMenu.class);
+
+
+}
